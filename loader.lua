4e3644dc865c117c282c7565d3580933ea168ca0
szze <106690765+CF-Trail@users.noreply.github.com>
2023-02-03 17:19:11 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,14 +1,16 @@
 local repo = 'https://raw.githubusercontent.com/CF-Trail/GameBreaker/main/games/'
 local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/main/Library.lua"))()
+local loadnx
 
-local function load()
+local function loader()
   task.spawn(function()
-    loadstring(game:HttpGet(repo .. tostring(game.PlaceId)))
+    loadnx = game:HttpGet(repo .. tostring(game.PlaceId))
   end)
 end
 
-if not pcall(load) then
+if not pcall(loader) then
      NotificationLibrary:SendNotification("Error", "Game is not supported", 3)
 else
      NotificationLibrary:SendNotification("Success", "Game supported", 3)
+     loadstring(loadnx)()
 end
