499b3c7e69b23aebbf6f3d6385c9a39f5b3eb946
szze <106690765+CF-Trail@users.noreply.github.com>
2023-02-09 23:04:56 +0300

    :sob: on god!!!!!!!!!!

--- loader.lua
+++ loader.lua
@@ -1,22 +1,22 @@
-local repo,gameRepo,NotificationLibrary,bool
+local bool
 task.spawn(function()
-repo = 'https://raw.githubusercontent.com/CF-Trail/GameBreaker/main/games/'
-gameRepo = 'https://raw.githubusercontent.com/CF-Trail/GameBreaker/main/games/gameIds/'
-NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/main/Library.lua"))()
-bool = false
+    task.wait(4)
+    if bool == true then
+        NotificationLibrary:SendNotification("Success", "Game supported", 4)
+    else
+        NotificationLibrary:SendNotification("Error", "Game is not supported", 4)  
+    end
+end)
+local repo = 'https://raw.githubusercontent.com/CF-Trail/GameBreaker/main/games/'
+local gameRepo = 'https://raw.githubusercontent.com/CF-Trail/GameBreaker/main/games/gameIds/'
+local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/main/Library.lua"))()
 pcall(function()
-       loadstring(game:HttpGet(repo .. tostring(game.PlaceId)))()
-       bool = true
+    loadstring(game:HttpGet(repo .. tostring(game.PlaceId)))()
+    bool = true
 end)
 if bool == false then
   pcall(function()
-       loadstring(game:HttpGet(gameRepo .. tostring(game.GameId)))()
-       bool = true
+    loadstring(game:HttpGet(gameRepo .. tostring(game.GameId)))()
+    bool = true
   end)
-end
-end)
-if bool == true then
-  NotificationLibrary:SendNotification("Success", "Game supported", 4)
-else
-  NotificationLibrary:SendNotification("Error", "Game is not supported", 4)  
 end
