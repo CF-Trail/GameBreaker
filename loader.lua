0e5fb06f96f897d42303f16bb5d18a82b46cc240
szze <106690765+CF-Trail@users.noreply.github.com>
2023-02-09 23:01:36 +0300

    oopsie x4

--- loader.lua
+++ loader.lua
@@ -1,21 +1,19 @@
+task.spawn(function()
 local repo = 'https://raw.githubusercontent.com/CF-Trail/GameBreaker/main/games/'
 local gameRepo = 'https://raw.githubusercontent.com/CF-Trail/GameBreaker/main/games/gameIds/'
 local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/main/Library.lua"))()
 local bool = false
 pcall(function()
-    task.spawn(function()
        loadstring(game:HttpGet(repo .. tostring(game.PlaceId)))()
-    end)
-    bool = true
+       bool = true
 end)
 if bool == false then
   pcall(function()
-    task.spawn(function()
        loadstring(game:HttpGet(gameRepo .. tostring(game.GameId)))()
-    end)
-    bool = true
+       bool = true
   end)
 end
+end)
 if bool == true then
   NotificationLibrary:SendNotification("Success", "Game supported", 4)
 else
